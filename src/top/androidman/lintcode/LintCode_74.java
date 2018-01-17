dd7bb7045b8d5735b45f8e6a152ebc7763fd69a9
weiyanjie <androidman@yeah.net>
2018-01-17 10:54:13 +0800

    修改完善

--- src/top/androidman/lintcode/LintCode_74.java
+++ src/top/androidman/lintcode/LintCode_74.java
@@ -18,12 +18,12 @@ public class LintCode_74 {
 	public static int findFirstBadVersionII(int n) {
 		// write your code here
 		int left = 1, right = n, mid = 0;
-		while (left <= right) {
+		while (left < right) {
 			//注意此处不建议用（right+left）/2   
 			//因为在边界Integer.MAX时，相加会出现mid为负数的情况
 			mid = left + (right - left) / 2;
 			if (SVNRepo.isBadVersion(mid)) {
-				right = mid - 1;
+				right = mid;
 			} else {
 				left = mid + 1;
 			}

4a5429d623f5c5364ce3e8839cdcf2740c293e34
weiyanjie <androidman@yeah.net>
2018-01-23 18:19:18 +0800

    背包问题

---
+++ src/top/androidman/lintcode/LintCode_92.java
@@ -0,0 +1,26 @@
+package top.androidman.lintcode;
+
+public class LintCode_92 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	public static int backPack(int m, int[] nums) {
+        // write your code here
+		int[] weights = new int[m];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < m; j++) {
+				
+			}
+		}
+		
+		
+		return 0;
+    }
+
+}
